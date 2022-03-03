b5f66f0843d4dea362d7439890a495291b7bbfb7
Tristan Poland <34868944+tristanpoland@users.noreply.github.com>
2022-03-02 20:50:47 -0500

    uploaded source code

---
+++ Guessing Game/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(166, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(466, 70)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Guess The Number"
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(85, 737)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(131, 40)
+        Me.BtnStart.TabIndex = 1
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(578, 737)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(131, 40)
+        Me.BtnExit.TabIndex = 2
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 30.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(807, 1007)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.Control
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents BtnExit As Button
+End Class
---
+++ Guessing Game/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        End
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        Dim Count, Number As Integer
+        Dim Correct As Boolean
+        Dim Guess As String
+
+        Randomize()
+        Number = Int(Rnd() * 100) + 1 'Selects Random Number
+        Correct = False
+        Count = 1
+
+        Do While Count < 11 And Correct = False 'Gives user 10 guesses
+            Guess = InputBox("Enter Guess", "Attempt " & Count & " of 10")
+            If Val(Guess) = Number Then 'If guess is correct set correct to true
+                Correct = True
+            Else
+                If Val(Guess) < Number Then
+                    MsgBox("Too Low", vbExclamation, "Attempt " & Count & " of 10")
+                ElseIf Guess > Number Then
+                    MsgBox("Too High", vbExclamation, "Attempt " & Count & " of 10")
+                End If
+
+                Count = Count + 1
+            End If
+        Loop
+        If Count < 11 Then
+            MsgBox("YAY You guessed it, have a coffee!. The correct guess was:" & Number)
+        ElseIf Count > 10 Then
+            MsgBox("Oops, You ran out of guesses!. The correct guess was:" & Number)
+        End If
+    End Sub
+End Class
